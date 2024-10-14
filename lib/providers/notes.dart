70289175f52fe2c6ca24452d6bf2c9bbb28859a4
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-10-14 16:27:30 +0100

    commit

--- lib/providers/notes.dart
+++ lib/providers/notes.dart
@@ -10,7 +10,7 @@ class NotesProvider with ChangeNotifier {
     });
   }
 
-  final List<Note> _notes = box.get('notes', defaultValue: []).cast<Note>();
+  final List<Note> _notes = box.get('notes', defaultValue: [], d);
   List<Note> get notes {
     return _notes..sort((a, b) => b.date.compareTo(a.date));
   }
