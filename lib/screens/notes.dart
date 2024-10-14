9241d6e7355585220509f70216be2cdf7e28be9f
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-10-14 14:45:40 +0100

    commit

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -214,13 +214,13 @@ class _NotesScreenState extends State<NotesScreen> {
                       child: Column(
                         children: [
                           Icon(
-                            IconlyLight.lock,
+                            Icons.move,
                             size: 20.sp,
                             color: color,
                           ),
                           6.sH,
                           Text(
-                            'Hide',
+                            'Move to',
                             style: TextStyle(
                               fontSize: 12.sp,
                               color: color,
