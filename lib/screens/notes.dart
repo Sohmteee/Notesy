65794ac7e5e8dc49d394bf97e98d086ce4ba91ba
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-10-14 17:08:58 +0100

    commit

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -130,48 +130,41 @@ class _NotesScreenState extends State<NotesScreen> {
             )
           : null,
       actions: [
-        context.watch<NotesProvider>().selectedNotes.isEmpty
-            ? IconButton(
-                icon: const Icon(
-                  IconlyLight.setting,
-                ),
-                onPressed: () {},
-              )
-            : IconButton(
-                icon: Icon(
-                  Icons.checklist_rtl_rounded,
-                  color: (context.read<NotesProvider>().selectedNotes.length ==
-                          context.read<NotesProvider>().notes.length +
-                              context.read<NotesProvider>().pinnedNotes.length)
-                      ? Theme.of(context).primaryColor
-                      : null,
-                ),
-                onPressed: () {
-                  if (context.read<NotesProvider>().selectedNotes.length ==
-                      (context.read<NotesProvider>().notes.length +
-                          context.read<NotesProvider>().pinnedNotes.length)) {
-                    context.read<NotesProvider>().clearSelectedNotes();
-                  } else {
-                    for (final note
-                        in context.read<NotesProvider>().pinnedNotes) {
-                      if (!context
-                          .read<NotesProvider>()
-                          .selectedNotes
-                          .contains(note)) {
-                        context.read<NotesProvider>().addSelectedNote(note);
-                      }
-                    }
-                    for (final note in context.read<NotesProvider>().notes) {
-                      if (!context
-                          .read<NotesProvider>()
-                          .selectedNotes
-                          .contains(note)) {
-                        context.read<NotesProvider>().addSelectedNote(note);
-                      }
-                    }
+        if (context.watch<NotesProvider>().selectedNotes.isEmpty)
+          IconButton(
+            icon: Icon(
+              Icons.checklist_rtl_rounded,
+              color: (context.read<NotesProvider>().selectedNotes.length ==
+                      context.read<NotesProvider>().notes.length +
+                          context.read<NotesProvider>().pinnedNotes.length)
+                  ? Theme.of(context).primaryColor
+                  : null,
+            ),
+            onPressed: () {
+              if (context.read<NotesProvider>().selectedNotes.length ==
+                  (context.read<NotesProvider>().notes.length +
+                      context.read<NotesProvider>().pinnedNotes.length)) {
+                context.read<NotesProvider>().clearSelectedNotes();
+              } else {
+                for (final note in context.read<NotesProvider>().pinnedNotes) {
+                  if (!context
+                      .read<NotesProvider>()
+                      .selectedNotes
+                      .contains(note)) {
+                    context.read<NotesProvider>().addSelectedNote(note);
                   }
-                },
-              ),
+                }
+                for (final note in context.read<NotesProvider>().notes) {
+                  if (!context
+                      .read<NotesProvider>()
+                      .selectedNotes
+                      .contains(note)) {
+                    context.read<NotesProvider>().addSelectedNote(note);
+                  }
+                }
+              }
+            },
+          ),
         16.sW,
       ],
     );
