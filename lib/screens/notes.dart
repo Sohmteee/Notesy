9a20a56479bf7950aa10e1ae7ccb7ee7a434ad4a
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-10-14 14:40:18 +0100

    commit

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -139,7 +139,7 @@ class _NotesScreenState extends State<NotesScreen> {
                 ),
                 onPressed: () {
                   if (context.read<NotesProvider>().selectedNotes.length ==
-                      context.read<NotesProvider>().notes.length) {
+                      (context.read<NotesProvider>().notes.length + context.read<NotesProvider>().pinnedNotes.length)) {
                     context.read<NotesProvider>().clearSelectedNotes();
                   } else {
                     for (final note in context.read<NotesProvider>().notes) {
